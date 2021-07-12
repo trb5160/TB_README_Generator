ad6db481d297e502e6338eef45e4e759687bdc3d
Tom Barber <trb5160@gmail.com>
2021-07-12 12:10:23 -0400

    deleted assets folder and updated utils code

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,37 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
-}
+// Function to generate markdown for README
+function generateMarkdown(answers) {
+  return  `
+  ## Title
+  ${answers.title}
+  ## Description 
+  ${answers.description}
+  
+  ## Table of Contents
+  1. [Installation](#installation)
+  2. [Usage](#usage)
+  3. [License](#license)
+  4. [Contributing](#contributing)
+  5. [Tests](#tests)
+  6. [Contact](#contact)
+  
+  ## Installation
+  ${answers.installation}
+  ## Usage
+  ${answers.usage}
+  ## License
+  ${answers.license}
+  ## Contributing
+  ${answers.contributing}
+  ## Tests
+  ${answers.tests}
+  ## Questions
+  See my contact info below if you have any questions.
+  ${answers.githubUserName}
+  ${answers.githubURL}
+  
+  ${answers.email}
+  
+`
+};
 
 module.exports = generateMarkdown;
